556a3d85e2a067f88c2a76e4ce753ed985d57d1f
Zubin Mehta <zubinpm@gmail.com>
2015-08-16 16:35:40 +0530

    zsh: Enhance and bug fixes

---
+++ zsh/options.zsh
@@ -0,0 +1,17 @@
+HISTSIZE=100000
+SAVEHIST=100000
+
+setopt ignoreeof
+
+# autoenv activation
+source /usr/local/bin/activate.sh
+
+
+# History
+export HISTFILE=~/.zsh_history
+export HISTSIZE=200000                 # huge internal buffer
+export SAVEHIST=200000                 # huge history file
+
+setopt extendedhistory
+
+
