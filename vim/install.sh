7e4d2d83ae73074bb34319aae1543f93043dde04
Zubin Mehta <zubinpm@gmail.com>
2015-08-17 12:06:11 +0530

    once: provision to run scripts only once

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,2 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim - +PluginInstall +qall
-
-cd ~/.vim/bundle/YouCompleteMe/
-./install.sh --clang-completer --gocode-completer
---
+++ vim/once.sh
@@ -0,0 +1,2 @@
+cd ~/.vim/bundle/YouCompleteMe/
+./install.sh --clang-completer --gocode-completer
