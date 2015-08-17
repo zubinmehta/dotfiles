1a5eba7616668a2ec126cbd690ac9903325d30b8
Zubin Mehta <zubinpm@gmail.com>
2015-08-17 13:12:12 +0530

    Tagbar and trivial fixes

--- vim/install.sh
+++ vim/install.sh
@@ -1,2 +1,6 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim - +PluginInstall +qall
+
+sudo apt-get install silversearcher-ag
+sudo apt-get install exuberant-ctags
+
