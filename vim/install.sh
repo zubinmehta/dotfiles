07a53adc6ea296287e611cec5bd07778b011d64c
Zubin Mehta <zubinpm@gmail.com>
2015-08-17 14:43:26 +0530

    Solarized vim theme: make it work flawlessly

--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,5 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim - +PluginInstall +qall
 
-sudo apt-get install silversearcher-ag
-sudo apt-get install exuberant-ctags
+sudo apt-get install -y silversearcher-ag exuberant-ctags
 
