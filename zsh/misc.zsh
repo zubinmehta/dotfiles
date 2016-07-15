3d15f7832273a548796ec9403bd7363ebaf14248
Zubin Mehta <zubinpm@gmail.com>
2016-07-15 11:05:53 +0530

    zsh alias addition and fixes

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -1,4 +1,5 @@
-alias y='youtube-dl -o "%(title)s.%(ext)s"'
+alias y='youtube-dl -o "%(title)s.%(ext)s" --ignore-errors'
+alias yy='youtube-dl -f 18/43/5/best -o "%(title)s.%(ext)s" --ignore-errors'
 
 alias e='espeak'
 alias ncmpcpp='ncmpcpp'
